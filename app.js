8736273eae447deff5262bb34f358dfc330a2af2
julhanlon <jhanlon289@gmail.com>
2020-07-30 13:03:54 -0700

    fixes

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var everySecond = new CronJob('* * * * *', function() {
 }, null, true, 'America/Los_Angeles');
 everySecond.start();
 
-
-
 var everyMinute = new CronJob('* * * * *', function() {
   console.log('You will see this message every minute');
 }, null, true, 'America/Los_Angeles');
 everyMinute.start();
+
+var everyHour = new CronJob('0 13 * * *', function() {
+    console.log('You will see this message at 1pm');
+  }, null, true, 'America/Los_Angeles');
+  everyHour.start();
